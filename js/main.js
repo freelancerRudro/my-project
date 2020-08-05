ea6a9e54b46223932399a26885416dcf91da15f3
RUDRO <freelancerrudro99@gmail.com>
2020-08-05 22:51:49 +0600

    project one added

---
+++ js/main.js
@@ -0,0 +1,75 @@
+function boxcolor1() {
+    var x = document.getElementById('box1').style.display;
+
+    if (document.getElementById('box1').style.display != "block") {
+        document.getElementById('box1').style.display = "block";
+    }else {
+        document.getElementById('box1').style.display = "none";
+    }
+}
+function color1() {
+    document.getElementById('colorbox1').style.backgroundColor = "red";
+    document.getElementById('box1').style.display = "none";
+}
+function color2() {
+    document.getElementById('colorbox1').style.backgroundColor = "yellow";
+    document.getElementById('box1').style.display = "none";
+}
+function color3() {
+    document.getElementById('colorbox1').style.backgroundColor = "aqua";
+    document.getElementById('box1').style.display = "none";
+}
+function color4() {
+    document.getElementById('colorbox1').style.backgroundColor = "#333";
+    document.getElementById('box1').style.display = "none";
+}
+function boxcolor2() {
+    var y = document.getElementById('box2').style.display;
+    
+    if (document.getElementById('box2').style.display!="block") {
+        document.getElementById('box2').style.display = "block";
+    }else {
+        document.getElementById('box2').style.display = "none";
+    }
+}
+function color1_1() {
+    document.getElementById('colorbox2').style.backgroundColor = "green";
+    document.getElementById('box2').style.display = "none";
+}
+function color2_1() {
+    document.getElementById('colorbox2').style.backgroundColor = "lime";
+    document.getElementById('box2').style.display = "none";
+}
+function color3_1() {
+    document.getElementById('colorbox2').style.backgroundColor = "black";
+    document.getElementById('box2').style.display = "none";
+}
+function color4_1() {
+    document.getElementById('colorbox2').style.backgroundColor = "#333";
+    document.getElementById('box2').style.display = "none";
+}
+function boxcolor3() {
+    var z = document.getElementById('box3').style.display;
+    
+    if (document.getElementById('box3').style.display!="block") {
+        document.getElementById('box3').style.display = "block";
+    }else {
+        document.getElementById('box3').style.display = "none";
+    }
+}
+function color1_2() {
+    document.getElementById('colorbox3').style.backgroundColor = "blue";
+    document.getElementById('box3').style.display = "none";
+}
+function color2_2() {
+    document.getElementById('colorbox3').style.backgroundColor = "brown";
+    document.getElementById('box3').style.display = "none";
+}
+function color3_2() {
+    document.getElementById('colorbox3').style.backgroundColor = "crimson";
+    document.getElementById('box3').style.display = "none";
+}
+function color4_2() {
+    document.getElementById('colorbox3').style.backgroundColor = "#333";
+    document.getElementById('box3').style.display = "none";
+}
